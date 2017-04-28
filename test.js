39bfdbfcc04d08475644a151d070bdcfd99c1be2
Vivek Poddar <vivekimsit@gmail.com>
2017-04-28 10:16:16 +0530

    Fix test case

--- test.js
+++ test.js
@@ -3,12 +3,6 @@ import test from 'ava';
 
 import m from '.';
 
-test('should be true', async t => {
-  const target = path.resolve(__dirname, 'fixtures', 'git-repo');
-  const isGitRepo = m(target);
-  t.is(await isGitRepo, true);
-});
-
 test('should be false', async t => {
   const target = path.resolve(__dirname, 'fixtures', 'non-git-repo');
   const isGitRepo = m(target);
